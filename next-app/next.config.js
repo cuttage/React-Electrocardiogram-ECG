54b9e7a8cf7320f70e539b7107cb1ef6bb931eb9
cuttage <martafattori@protonmail.ch>
2023-06-13 08:47:47 +0200

    Deploy Changes

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -5,6 +5,7 @@ const withTM = require('next-transpile-modules')(['@arction/lcjs'])
 module.exports = withTM({
   // Other Next.js configurations
   reactStrictMode: false,
+  target: 'serverless',
   webpack(config) {
     config.resolve.extensions.push('.ts', '.tsx')
     return config
